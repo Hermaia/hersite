f8edd922223cb3b9be428fd72153e7039d452c81
Toshiaki Inahata <darwin49@gmail.com>
2016-11-05 21:51:18 +0900

    add d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,7 @@
 /// <reference path="globals/cheerio/index.d.ts" />
 /// <reference path="globals/marked/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/power-assert/index.d.ts" />
 /// <reference path="globals/pug/index.d.ts" />
+/// <reference path="globals/sinon/index.d.ts" />
